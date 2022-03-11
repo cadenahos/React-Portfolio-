aa9e5027a267fd10e20c6200eec5578513f9ed62
Juan Pablo Cadena <juanpablocadena@MacBook-Air-de-Juan.local>
2022-03-11 05:25:03 -0500

    [UPDATE] pull apart  card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@styles/Card.scss';
+import mookImage from '@images/magazine.jpg';
+import javascriptLogo from '@logos/java-script.svg';
+import nodeLogo from '@logos/nodejs.svg';
+import cssLogo from '@logos/css.svg';
+
+const Card = () => {
+    return (
+        <div className="card">
+        <div className="card-presentation card-presentation--project">
+            <h4 className="card-presentation--title">Cultural Magazine</h4>
+            <img src={mookImage} alt="magazine" />
+            <div className="card-presentation--project--lenguages">
+                <h4>Languages</h4>
+                <div className="card-presentation--project--lenguages--icons">
+                    <img src={javascriptLogo} alt=""/>
+                    <img src={nodeLogo} alt="" />
+                    <img src={cssLogo} alt="" />
+                </div>
+            </div>
+        </div>
+        <div className="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis
+                quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum
+                accusantium ea eveniet?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum
+                officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur
+                voluptates unde ut at?
+            </p>
+        </div>
+    </div>
+    );
+};
+
+export default Card;
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import '@styles/Projects.scss';
-import mookImage from '@images/magazine.jpg';
-import javascriptLogo from '@logos/java-script.svg';
-import nodeLogo from '@logos/nodejs.svg';
-import cssLogo from '@logos/css.svg';
+import Card from '../components/Card';
 
 const Projects = () => {
+    const projectList = [
+        {
+            'title': 'CULTURA MAGAZINE',
+            'image': '',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum accusantium ea eveniet? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur voluptates unde ut at?',
+            'languages': [
+                {
+                    'name': 'js',
+                    'url': ''
+                }
+            ]
+        },
+        {
+            'title': 'CULTURA MAGAZINE',
+            'image': '',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum accusantium ea eveniet? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur voluptates unde ut at?',
+            'languages': [
+                {
+                    'name': 'js',
+                    'url': ''
+                }
+            ]
+        },
+        {
+            'title': 'CULTURA MAGAZINE',
+            'image': '',
+            'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum accusantium ea eveniet? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur voluptates unde ut at?',
+            'languages': [
+                {
+                    'name': 'js',
+                    'url': ''
+                }
+            ]
+        }
+    ]
+        
     return (
         <section className="projects">
         <div className="home-section-title" >
@@ -13,75 +46,11 @@ const Projects = () => {
             <div></div>
         </div>
             <article className="projects-container ">
-                <div className="card">
-                    <div className="card-presentation card-presentation--project">
-                        <h4 className="card-presentation--title">Cultural Magazine</h4>
-                        <img src={mookImage} alt="magazine" />
-                        <div className="card-presentation--project--lenguages">
-                            <h4>Languages</h4>
-                            <div className="card-presentation--project--lenguages--icons">
-                                <img src={javascriptLogo} alt=""/>
-                                <img src={nodeLogo} alt="" />
-                                <img src={cssLogo} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-description">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis
-                            quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum
-                            accusantium ea eveniet?
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum
-                            officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur
-                            voluptates unde ut at?
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-presentation card-presentation--project">
-                        <h4 className="card-presentation--title">Cultural Magazine</h4>
-                        <img src={mookImage} alt="magazine" />
-                        <div className="card-presentation--project--lenguages">
-                            <h4>Languages</h4>
-                            <div className="card-presentation--project--lenguages--icons">
-                                <img src={javascriptLogo} alt=""/>
-                                <img src={nodeLogo} alt="" />
-                                <img src={cssLogo} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-description">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis
-                            quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum
-                            accusantium ea eveniet?
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum
-                            officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur
-                            voluptates unde ut at?
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-presentation card-presentation--project">
-                        <h4 className="card-presentation--title">Cultural Magazine</h4>
-                        <img src={mookImage} alt="magazine" />
-                        <div className="card-presentation--project--lenguages">
-                            <h4>Languages</h4>
-                            <div className="card-presentation--project--lenguages--icons">
-                                <img src={javascriptLogo} alt=""/>
-                                <img src={nodeLogo} alt="" />
-                                <img src={cssLogo} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-description">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias saepe quas eligendi, officiis
-                            quae amet culpa fugit ex aliquid deleniti rem ipsam soluta commodi qui nulla harum
-                            accusantium ea eveniet?
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum soluta vel amet hic! A dolorum
-                            officia aperiam veniam. Qui tenetur magni doloremque officia veniam facere pariatur
-                            voluptates unde ut at?
-                        </p>
-                    </div>
-                </div>
+                {
+                    projectList.map((item,index) => {
+                        return <Card key={`project${index}`}/>
+                    })
+                }
             </article>
         </section>
     );
