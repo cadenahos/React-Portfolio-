5013097ce462e18be62ab981e0c704544733f597
Juan Pablo Cadena <juanpablocadena@MacBook-Air-de-Juan-2.local>
2022-10-03 23:46:04 -0500

    [fix] biography part

--- src/containers/Bio.jsx
+++ src/containers/Bio.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import fotoperfil from '@images/perfilcentrado.jpeg';
+import Typewriter from 'typewriter-effect';
 import '@styles/Bio.scss';
+  
 
 const Bio = () => {
+
+    React.useEffect(()=>{
+    },[])
     return (
         <section className="bio">
         <div className="home-section-title" >
@@ -10,13 +15,35 @@ const Bio = () => {
             <div></div>
         </div>
         <div className="bio-container">
-            <img className="bio-fotoPerfil" src={fotoperfil} alt="foto perfil"/>
+            {/* <img className="bio-fotoPerfil" src={fotoperfil} alt="foto perfil"/> */}
             <div className="bio-description">
+            <div className='bio-hello'>
+
+                <Typewriter 
+  onInit={(typewriter) => {
+    typewriter.typeString('Under construction!')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .deleteAll()
+      .callFunction(() => {
+        console.log('All strings were deleted');
+      })
+      .start();
+  }}
+/>
+            </div>
                 <p>                  
-                    I am the type of person who feel happy working with modern technology. I love to work in graphic interfaces and approach new ways of solving problems.
-                    I consider myself an avid learner who enjoy reading technical blogs and books in subject that I am interested as, frontend, algorithms, and Management.
-                </p>
-                <button className="button--primary">More info ...</button>
+                    I am the type of person who feel happy working with modern technology. 
+                    <br></br>
+                    <br></br>
+                    
+                    I love to work in graphic interfaces and approach new ways of solving problems.
+                    <br></br>
+                    <br></br>
+                    </p>
+                {/* <button className="button--primary">More info ...</button> */}
             </div>
         </div>       
         </section>
